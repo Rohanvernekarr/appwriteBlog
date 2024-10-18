267bc0232c9a7240b1c5bc969de95e39e4b5b2de
Rohan Vernekar <153760875+Rohanvernekarr@users.noreply.github.com>
2024-10-18 09:33:49 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Signup from "./Signup";
+import SignUp from "./SignUp";
 import Login from "./Login";
 import Button from "./Button";
 import PostForm from "./post-form/PostForm";
@@ -19,7 +19,7 @@ export {
     Logo,
     LogoutBtn,
     RTE,
-    Signup,
+    SignUp,
     Login,
     Button,
     PostForm,
